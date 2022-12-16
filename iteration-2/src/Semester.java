1268bfedbf639b5f6ce0b7c4170ff713429e6e7d
enessagroglu <enes-2001sagroglu@hotmail.com>
2022-12-16 23:44:35 +0300

    schedule and semester added

--- iteration-2/src/Semester.java
+++ iteration-2/src/Semester.java
@@ -1,40 +1,53 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Semester {
-    public static int semesterNo;
-    private String semesterName;
+    private int semesterId;
+    private List<Course> courses = new ArrayList<>();
+    private String season;
 
-    public String getSemesterName() {
-        return defineSemesterName(getSemesterNo());
+    public Semester(int semesterId, String season) {
+        this.semesterId = semesterId;
+        this.season = season;
     }
 
-    public int getSemesterNo() {
-        return semesterNoGenerate();
+    public int getSemesterId() {return semesterId;}
+
+    public void setSemesterId(Integer semesterId) {this.semesterId = semesterId;}
+
+    public List<Course> getCourses() {return courses;}
+
+    public void setCourses(List<Course> courses) {this.courses = courses;}
+
+    public String getSeason() {return season;}
+
+    public void setSeason(String season) {this.season = season;}
+
+
+    public void addCourse(Course course) {courses.add(course);}
+    @Override
+    public boolean equals(Object o) {
+        // If the two objects are the same object, return true.
+        // This check is necessary because we want to ensure that the objects are actually
+        // the same object and not just two objects with the same data.
+        if (this == o) return true;
+        // If the object passed as an argument is not an instance of the Semester class,
+        // return false. This check is necessary because we only want to compare Semester
+        // objects, not objects of other types.
+        if (!(o instanceof Semester)) return false;
+        // Cast the object passed as an argument to the Semester type so that we can access
+        // its fields and methods.
+        Semester semester = (Semester) o;
+        // Compare the semesterId field of the two Semester objects. If they are equal,
+        // return true. Otherwise, return false.
+        return getSemesterId().equals(semester.getSemesterId());
     }
 
+    @Override
+    public int hashCode() {return getSemesterId().hashCode();}
 
-    private void setSemesterName() {
-        return;
-    }
-
-    public int compareTo(Semester obj) {
-        return 1;
-    }
-
+    @Override
     public String toString() {
-        return "";
-    }
-    public static int semesterNoGenerate(){
-        Random random=new Random();
-        semesterNo=random.nextInt(8-1)+1;
-        return semesterNo;
-    }
-    public String defineSemesterName(int semesterNo){
-        if(semesterNo%2 == 0)
-            semesterName = "SPRING";
-        else
-            semesterName = "FALL";
-        return semesterName;
-
+        return "Semester{" + "semesterId=" + semesterId + '}';
     }
 }
