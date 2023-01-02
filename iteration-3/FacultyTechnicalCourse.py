b3d9d500627f824b673668d269d004e02d31c1e0
enessagroglu <enes-2001sagroglu@hotmail.com>
2023-01-02 18:31:55 +0300

    kurslardaki students kaldırıldı

--- iteration-3/FacultyTechnicalCourse.py
+++ iteration-3/FacultyTechnicalCourse.py
@@ -2,4 +2,4 @@ from Course import Course
 
 class FacultyTechnicalCourse(Course):
     def __str__(self):
-        return f"FacultyTechnicalCourse(course_id='{self._courseId}', name='{self._name}', course_code='{self._courseCode}', required_credits={self._requiredCredits}, semester_number={self._semesterNumber}, course_type='{self._courseType}', course_current_student_number={self._courseCurrentStudentNumber}, semester='{self._semester}', quota={self._quota}, credit={self._credit}, session_count={self._sessionCount}, sessions='{self._sessions}', prerequisites='{self._prerequisites}', students='{self._students}')"
+        return f"FacultyTechnicalCourse(course_id='{self._courseId}', name='{self._name}', course_code='{self._courseCode}', required_credits={self._requiredCredits}, semester_number={self._semesterNumber}, course_type='{self._courseType}', course_current_student_number={self._courseCurrentStudentNumber}, semester='{self._semester}', quota={self._quota}, credit={self._credit}, session_count={self._sessionCount}, sessions='{self._sessions}', prerequisites='{self._prerequisites}')"
