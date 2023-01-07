1981c3801df6ff4b4890d35c9438d34c124a414d
Emre Demir <emredemir20@marun.edu.tr>
2023-01-07 14:21:52 +0300

    UncompletedCreditChecker class test

---
+++ iteration-3/UncompletedCreditChecker.py
@@ -0,0 +1,16 @@
+class UncompletedCreditError(Error):
+    def init(self, student, course):
+        super().init(student)
+        self.course = course
+
+    def get_course(self):
+        return self.course
+
+    def set_course(self, course):
+        self.course = course
+
+    def raise_course(self):
+        return self.course
+
+    def raise_error(self):
+        return f"The advisor didn't approve {self.course.get_course_id()} because student completed credits < 155"
