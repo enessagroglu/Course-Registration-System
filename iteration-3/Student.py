b4aa8e3dcd80a0ab7c3bfce2ae92e05d55c18d05
Ömercan Sabun <omercnsbn@icloud.com>
2023-01-09 03:33:52 +0300

    Update Student.py
    added try-catch blocks, error log.

--- iteration-3/Student.py
+++ iteration-3/Student.py
@@ -98,29 +98,47 @@ class Student(Person):
 
     
 
-    def calculateCumulativeGpa(self):
+def calculateCumulativeGpa(self):
+    try:
         transcript = Transcript()
         cCredit = transcript.cumulativeCredit
         cGrade = transcript.cumulativeCredit
         result = cGrade/cCredit
         self._cgpa = result
-
+    except Exception as e:
+        print(e)
+    except ZeroDivisionError as z:
+        print(z)
     
     def printNonTakenCourses(self):
-        for course in self._nonTakenCourses:
-            print(course)
-        
+        try:
+            for course in self._nonTakenCourses:
+                print(course)
+        except Exception as e:
+            print(e)
+
     def printFailedCourses(self):
-        for course in self._failedCourses:
-            print(course)
+        try:
+            for course in self._failedCourses:
+                print(course)
+        except Exception as e:
+            print(e)
 
     def printSelectedSessions(self):
-        for courseSession in self._selectedSessions:
-            print(courseSession)
+        try:
+            for courseSession in self._selectedSessions:
+                print(courseSession)
+        except Exception as e:
+            print(e)
+
 
     def addNonTakenCourse(self,course: Course):
+    try:
         if course not in self.nonTakenCourses:
             self.nonTakenCourses.append(course)
+    except Exception as e:
+        print(e)
+
 
     def __str__(self):
         return f"Student(name='{self._name}', surname='{self._surname}', id={self._id}, emails='{self._email}', semester='{self._semester}',  transcript='{self._transcript}', selected_courses='{self._selectedCourses}', selected_sessions='{self._selectedSessions}', entry_year={self._entryYear}, schedule='{self._schedule}', cgpa={self._cgpa}, active_courses='{self._activeCourses}', past_courses='{self._pastCourses}', non_taken_courses='{self._nonTakenCourses}', failed_courses='{self._failedCourses}')"
